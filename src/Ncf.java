211a53f474a7f67a28e2b5eead9e6c8e256991f9
BoikoAnton <boyko.anton@mail.ru>
2017-03-23 22:55:17 +0200

    Initial commit

---
+++ src/Ncf.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+
+/**
+ * Created by BoikoAnton on 23.03.2017.
+ */
+public class Ncf {
+    public static void main(String[] args){
+        Scanner asd = new Scanner(System.in);
+       String ln = asd.nextLine();
+        System.out.println("Hello " + ln);
+
+
+    }
+}
